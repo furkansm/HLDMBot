e2699dd71b54fb8a2b790bde7df9a65f9d364aa4
Furkan Bilgin <info@furkanbilgin.net>
2022-08-05 18:58:58 +0300

    Disable update hud

--- cl_dll/hud_watermark.cpp
+++ cl_dll/hud_watermark.cpp
@@ -15,7 +15,7 @@ int CHudWatermark::VidInit()
 {
 	m_iFlags |= HUD_ACTIVE;
 	refresh_draw_until = true;
-	update_is_available = update_checker::is_update_available();
+	//update_is_available = update_checker::is_update_available();
 
 	return 1;
 }
@@ -35,11 +35,10 @@ int CHudWatermark::Draw(float time)
 	int r, g, b;
 	UnpackRGB(r, g, b, gHUD.m_iDefaultHUDColor);
 
-	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight, "OpenAG client build " __DATE__, r, g, b);
-	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 2, "j.mp/OpenAG", r, g, b);
+	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight, "HLDMBot client build " __DATE__, r, g, b);
 
-	if (update_is_available)
-		gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight / 2 * 7, "An update is available.", r, g, b);
+	//if (update_is_available)
+	//	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight / 2 * 7, "An update is available.", r, g, b);
 
 	return 0;
 }
